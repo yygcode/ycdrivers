df7ccc7fdc9366fe54f5dc52748ad2a7b2ae2e5c
yanyg <yygcode@gmail.com>
2012-04-24 20:41:13 +0800

    rename ycsysfs and ycbus

---
+++ 02-sysfs/kset.c
@@ -0,0 +1,15 @@
+/*
+ * demo kset
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kobject.h>
+
+static int __init yckset_init(void)
+{
+}
+
+static void __exit yckset_exit(void)
+{
+}
